7452434877c7098a6d04052dbb3a0ea3940a45a9
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-03-30 23:32:30 -0500

    finished HTML, added most of java, need to finish uv, 5day, and list

--- javascript.js
+++ javascript.js
@@ -1,6 +1,62 @@
 $(document).ready(function () {
-    $(function () {
-        $("#div1").append('<div>' + "new div" + '</div>');
+
+    var apiKey = "08b0dd9f10a03bff1d46baba879a4a39"
+    
+    
+    $("#getWeather").on("click", function() {
+        var userInput = $("#city-search").val();
+        displayWeather(userInput);
+      });
+
+    var cities = [];
+
+    function displayWeather(city) {
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: "json"
+        }).then(function (response) {
+            console.log(response);
+
+            var city= response.name
+            $("#cityName").text(city)
+
+            var temp = Math.floor((response.main.temp - 273.15) * 1.80 + 32)
+
+            $("#curTemp").text("Temp: "+temp+ " F")
+
+            var humidity= response.main.humidity
+            $("#curHum").text("Humidity: "+humidity)
+
+            var wind= response.wind.speed
+            $("#curWind").text("Wind Speed: "+wind + " MPH")
+
+            
+            // cloud cover,ozone thickness,time of year,elevation
+            var lat= response.coord.lon;
+            var lon= response.coord.lat;
+
+            $.ajax({
+                url:"http://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+ "&lat="+ lat + "&lon="+ lon,
+                method: "GET",
+                dataType:"json"
+                
+            }).then(function(response1){
+                console.log(response1)
+
+                // var uv=
+            // $("#curUv").text(uv)
+                
+            })
+        });
+        
     }
-    )
-})
+
+    
+
+
+
+});
